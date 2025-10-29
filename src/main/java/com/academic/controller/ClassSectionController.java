fcb9a024053650a9155cb280b25aed32fd51b031
AKSHAYBARMATE <100841949+AKSHAYBARMATE@users.noreply.github.com>
2025-10-29 11:34:48 +0530

    Change API endpoint prefix for ClassSectionController

--- src/main/java/com/academic/controller/ClassSectionController.java
+++ src/main/java/com/academic/controller/ClassSectionController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/sms/api/v1/academic-module")
+@RequestMapping("/api/v1/academic-module")
 @RequiredArgsConstructor
 @Slf4j
 public class ClassSectionController {
