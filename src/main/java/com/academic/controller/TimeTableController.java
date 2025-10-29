e61af238b842495556224cebda2ef1fce3cbd5de
AKSHAYBARMATE <100841949+AKSHAYBARMATE@users.noreply.github.com>
2025-10-29 11:35:47 +0530

    Update TimeTableController.java

--- src/main/java/com/academic/controller/TimeTableController.java
+++ src/main/java/com/academic/controller/TimeTableController.java
@@ -15,7 +15,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/sms/api/v1/academic-module")
+@RequestMapping("/api/v1/academic-module")
 @RequiredArgsConstructor
 @Slf4j
 public class TimeTableController {
