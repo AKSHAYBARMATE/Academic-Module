538afd46cf979db8fc45c06b82ce8537aa2beff1
atharv0809 <atharv.deshpande@sarvosmi,io>
2026-02-12 09:50:16 +0530

    add response class

---
+++ src/main/java/com/academic/response/MarksheetResponse.java
@@ -0,0 +1,27 @@
+package com.academic.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class MarksheetResponse {
+    private Long id;
+    private Long studentId;
+    private String studentName;
+    private String className;
+    private String sectionName;
+    private String sessionName;
+    private String examTypeName;
+    private LocalDate examDate;
+    private Integer totalMarksObtained;
+    private Integer totalMaxMarks;
+    private Double percentage;
+    private String grade;
+    private Boolean published;
+}
