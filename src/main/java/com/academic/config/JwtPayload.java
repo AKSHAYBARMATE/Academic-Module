b5cfc9db794753aaadc3bbefde3d86c875d55dc6
atharv0809 <atharv.deshpande@sarvosmi,io>
2026-04-14 01:22:11 +0530

    add ignore property

--- src/main/java/com/academic/config/JwtPayload.java
+++ src/main/java/com/academic/config/JwtPayload.java
@@ -1,8 +1,10 @@
 package com.academic.config;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class JwtPayload {
     private String name;
     private String userType;
