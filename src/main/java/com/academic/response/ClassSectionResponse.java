107967f6f110789b1ae45f3436386c8fda790703
ContextAI India <manmohan.shrivas@sarvosmi.io>
2026-07-02 14:54:51 +0530

    error and missing field added 2

--- src/main/java/com/academic/response/ClassSectionResponse.java
+++ src/main/java/com/academic/response/ClassSectionResponse.java
@@ -18,7 +18,7 @@ public class ClassSectionResponse {
     private String sectionName;   // resolved from common master
     private Integer sectionId;    // original ID
     private String classTeacher;
-    private Integer classTeacherId;
+    private Long classTeacherId;
 
     private Integer students;
     private String roomNo;
